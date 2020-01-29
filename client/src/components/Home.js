6416a76c4aa999313844af80f8887eca3ec6055a
Carl Wicker <carlwicker@gmail.com>
2020-01-29 00:19:03 +0000

    meh

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,9 @@ function Home(props) {
   return (
     <div class="homeContainer">
       <h1 class="display-4">Work In Progress</h1>
-      <p class="lead">Playing around with React Router.</p>
+      <p class="lead">
+        Playing around with Express, React, Redux, Axios, Router...
+      </p>
 
       <Link to="/create">
         <button className="btn btn-light btn-lg">Create Account </button>
