a46f232397864c5fbc25428ecf72765fa3c160b4
Carl Wicker <carlwicker@gmail.com>
2020-02-09 16:00:07 +0000

    added edit user form

--- client/src/components/EditUser.js
+++ client/src/components/EditUser.js
@@ -7,7 +7,47 @@ function EditUser() {
   return (
     <div className="editUserContainer">
       <h1 className="display-4">Edit User</h1>
-      <h2>ID: {_id}</h2>
+
+      <form>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder={_id}
+            readOnly
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="First Name"
+          />
+        </div>
+        <div className="form-group">
+          <input className="form-control" type="text" placeholder="Last Name" />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="email"
+            placeholder="Email Address"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" className="btn btn-danger" disabled>
+          Update
+        </button>{" "}
+        <button type="button" className="btn btn-primary">
+          Revert
+        </button>
+      </form>
     </div>
   );
 }
