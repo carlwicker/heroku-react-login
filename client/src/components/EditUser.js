8ae84de7306bdc5dcb595b9d8cff2210faf18662
Carl Wicker <carlwicker@gmail.com>
2020-02-10 21:26:58 +0000

    edit user form added

--- client/src/components/EditUser.js
+++ client/src/components/EditUser.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { useParams, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function EditUser() {
-  // const { _id } = useParams();
   const user = useSelector(state => state.getUserReducer);
 
   let editForm = {
@@ -17,70 +15,68 @@ function EditUser() {
   return (
     <div className="editUserContainer">
       <h1 className="display-4">Edit User</h1>
-      <div className="form-group">
-        <input className="form-control" placeholder={editForm._id} readOnly />
-      </div>
-      <div className="form-group">
-        <input
-          className="form-control"
-          type="text"
-          placeholder="First Name"
-          defaultValue={editForm.firstname}
-          onChange={e => {
-            editForm.firstname = e.target.value;
-            console.log(editForm.firstname);
+      <form>
+        <div className="form-group">
+          <input className="form-control" placeholder={user._id} readOnly />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="First Name"
+            defaultValue={user.firstname}
+            onChange={e => {
+              user.firstname = e.target.value;
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Last Name"
+            defaultValue={user.lastname}
+            onChange={e => {
+              user.lastname = e.target.value;
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="email"
+            placeholder="Email Address"
+            defaultValue={user.email}
+            onChange={e => {
+              user.email = e.target.value;
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="password"
+            placeholder="Password"
+            autoComplete="current"
+            defaultValue={editForm.password}
+            onChange={e => {
+              editForm.password = e.target.value;
+            }}
+          />
+        </div>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={() => {
+            console.log(editForm);
           }}
-        />
-      </div>
-      <div className="form-group">
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Last Name"
-          defaultValue={editForm.lastname}
-          onChange={e => {
-            editForm.lastname = e.target.value;
-            console.log(editForm.lastname);
-          }}
-        />
-      </div>
-      <div className="form-group">
-        <input
-          className="form-control"
-          type="email"
-          placeholder="Email Address"
-          defaultValue={editForm.email}
-          onChange={e => {
-            editForm.email = e.target.value;
-            console.log(editForm.email);
-          }}
-        />
-      </div>
-      <div className="form-group">
-        <input
-          className="form-control"
-          type="password"
-          placeholder="Password"
-          autoComplete="current"
-          defaultValue={editForm.password}
-          onChange={e => {
-            editForm.password = e.target.value;
-            console.log(editForm.password);
-          }}
-        />
-      </div>
-      <button
-        type="button"
-        className="btn btn-danger"
-        onClick={() => {
-          console.log(editForm);
-        }}
-      >
-        Update
-      </button>{" "}
-      <button type="button" className="btn btn-primary">
-        Revert
-      </button>
+        >
+          Update
+        </button>{" "}
+        <button type="button" className="btn btn-primary">
+          Revert
+        </button>
+      </form>
     </div>
   );
 }
